1a76ed2eefa087d35841ab63a275a29129d5d791
Charit <charithnanduri03@gmail.com>
2022-03-05 01:01:00 -0500

    route guarding and firebase rules changes

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from "../views/auth/Login.vue";
 import Signup from "../views/auth/Signup.vue";
 import CreatePlaylist from "@/views/playlist/CreatePlaylist.vue";
 import PlaylistDetails from "@/views/playlist/PlaylistDetails.vue";
+import UserPlaylists from "@/views/playlist/UserPlaylists.vue";
 import { projectAuth } from "@/firebase/config";
 
 //create route guard
@@ -17,35 +18,52 @@ const requireAuth = (to, from, next) => {
   }
 };
 
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if (user) {
+    next({ name: "Home" });
+  } else {
+    next();
+  }
+};
+
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
-    beforeEnter: requireAuth
+    beforeEnter: requireAuth,
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    beforeEnter: requireNoAuth
   },
   {
     path: "/signup",
     name: "Signup",
     component: Signup,
+    beforeEnter: requireNoAuth
   },
   {
     path: "/playlists/create",
     name: "CreatePlaylist",
     component: CreatePlaylist,
-    beforeEnter: requireAuth
+    beforeEnter: requireAuth,
   },
   {
     path: "/playlists/:id",
     name: "PlaylistDetails",
     component: PlaylistDetails,
     beforeEnter: requireAuth,
-    props:true
+    props: true,
+  },
+  {
+    path: "/playlists/user",
+    name: "UserPlaylists",
+    component: UserPlaylists,
+    beforeEnter: requireAuth,
   },
 ];
 
