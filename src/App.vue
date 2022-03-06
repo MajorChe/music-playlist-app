e3f88d8e4517d0de5161f3fce57aa6529c6bc13a
Charit <charithnanduri03@gmail.com>
2022-03-05 22:51:50 -0500

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <Navbar/>
+  <Navbar />
   <div class="content">
     <router-view />
   </div>
+  <div class="footer"><p><b>Created by Charit</b></p></div>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue"
-export default ({
+import Navbar from "@/components/Navbar.vue";
+export default {
   components: { Navbar },
-  setup() {
-  },
-})
+  setup() {},
+};
 </script>
-
 
 <style>
 .content {
   margin: 0 auto;
   max-width: 1200px;
   padding: 0 20px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  text-align: center;
+  background-color: #f3f1f1;
+  color: black;
+}
+.footer p {
+  text-align: center;
+  padding-top: 20px;
 }
 </style>
